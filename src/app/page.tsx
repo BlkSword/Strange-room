f49eac5143af768b9b5957b89512aebfbce64d51
XiaoHei <148538236+BlkSword@users.noreply.github.com>
2026-09-12 13:45:56 +0800

    chore: remove the old implementation and keep internal docs untracked

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Home from '@/components/home';
-
-export default function Chat() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
-}
--- src/lib/export/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 房间导出/导入模块
- */
-
-export * from './crypto-utils';
-export * from './room-exporter';
